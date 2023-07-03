c35d854daef4aad567b30f5b886d54a83dd3d7a2
Pritam Prasanjeet Behera <99427618+Pritam08pb@users.noreply.github.com>
2023-07-03 21:38:43 +0530

    Update scrapy.py

--- scrapy.py
+++ scrapy.py
@@ -1,3 +1,5 @@
+import schedule
+import time
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -5,7 +7,7 @@ from datetime import datetime, timedelta
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-otime=""
+
 # Scraping weather
 def scrape():
     url = "https://mausam.imd.gov.in/bhubaneswar/" 
@@ -14,43 +16,35 @@ def scrape():
     temperature = soup.find("div", class_="temp").text
     wind = soup.find("li", class_="sun-info").text
     otime = soup.find("small", class_="small").text
+    hum = soup.find("div", id="temperature1").text
+
     temperature = temperature[1:3]
     wind = wind
     otime = str(otime[18:])
-    return temperature,wind,otime
-
-def store_weather_data(temperature, wind, otime):
-    data = [[temperature, wind, otime, datetime.now().strftime("%Y-%m-%d")]]
+    hum = hum[1:]
+    data = [[temperature, wind,hum, otime, datetime.now().strftime("%Y-%m-%d")]]
     with open("weather_dataset.csv", "r") as file:
         reader = csv.reader(file)
         last_row = list(reader)[-1]
-        time = str(last_row[2])
+        time = str(last_row[3])
         if otime != time:
              with open("weather_dataset.csv", "a", newline="") as file:
                writer = csv.writer(file)
                writer.writerows(data)
-
-# Load weather dataset from CSV
-def load_weather_dataset():
-    dataset = []
-    with open("weather_dataset.csv", "r") as file:
-        reader = csv.reader(file)
-        next(reader)  # Skip header row
-        for row in reader:
-            dataset.append(row)
-    return dataset
+    return temperature,wind,hum,otime
 
 
+
+# def schedule_scraping():
+#     schedule.every(1).minutes.do(scrape)
+#     while True:
+#         schedule.run_pending()
+#         time.sleep(1)
 
 
 def main():
-    temperature,wind,otime= scrape()
-    store_weather_data(temperature, wind, otime)
-    dataset = load_weather_dataset()
-    # predict_weather(dataset)
-    
+    temperature, wind, hum, otime = scrape()
+
 if __name__ == "__main__":
     main()
-
-
-
+    #schedule_scraping()
