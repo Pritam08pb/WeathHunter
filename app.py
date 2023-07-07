d744b0aa711040181e4038363d2560bc29d1eb11
Pritam Prasanjeet Behera <99427618+Pritam08pb@users.noreply.github.com>
2023-07-07 15:35:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, jsonify
 import scrapy as sc
 from flask_cors import CORS
+import schedule
+import time
+import requests
+from bs4 import BeautifulSoup
+import csv
+import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -9,17 +15,30 @@ CORS(app)
 @app.route("/", methods=["POST", "GET"])
 def predict_weather():
     # Scrape weather data
-    temperature, wind, hum, otime = sc.scrape()
-    sc.data(temperature, wind, hum, otime)
 
-    predicted_weather = "down"
-  
+    url = "https://mausam.imd.gov.in/bhubaneswar/" 
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    temperature = soup.find("div", class_="temp").text
+    wind = soup.find("li", class_="sun-info").text
+    otime = soup.find("small", class_="small").text
+    hum = soup.find("div", id="temperature1").text
+    current_datetime = datetime.datetime.now()
+
+    temperature = temperature[1:3]
+    wind = wind
+    otime = str(otime[18:])
+    hum = hum[1:]
+    current_date = current_datetime.date()
+    
+    
     # Create a response dictionary
     response = {
         "temperature": temperature,
         "wind": wind,
         "humidity": hum,
-        "predicted_weather": predicted_weather
+        "observation_time": otime
+        "current_date": current_date
     }
 
     return jsonify(response)
